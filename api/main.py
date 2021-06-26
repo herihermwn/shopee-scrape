eaa372537488735bb227ca06f06ecbff590a56a1
herihermwn <herih1876@gmail.com>
2021-06-26 19:00:22 +0700

    Update: API Sample

--- api/main.py
+++ api/main.py
@@ -1,45 +1,11 @@
 import json
+import requests
 import logging
 from config import *
 from bottle import run, post, Bottle, request, response
-from scrape_shopee.spiders.shopee_spider import ShopeeSpiderSpider
-from multiprocessing import Process, Manager
-from twisted.internet import reactor
-from scrapy.crawler import CrawlerProcess
 from datetime import datetime
 
 
-# Using CrawlerProcess will display the spider LOGGER
-def getUserTweets(keyword):
-    # Setting for spider
-    settings = {
-        'LOG_FILE': 'logs/logs-{}.txt'.format(datetime.now())
-    }
-    crawler = CrawlerProcess(settings=settings)
-
-    # Set spider to CrawlerProcess & Passing parameter to Spider
-    deferred = crawler.crawl(ShopeeSpiderSpider, keyword=keyword)
-    deferred.addBoth(lambda _: reactor.stop())
-    reactor.run()
-
-    @deferred.addCallback
-    def _success(results):
-        """
-        Do something when success
-        """
-        logging.info("Success")
-
-    @deferred.addErrback
-    def _error(failure):
-        """
-        Do something when error
-        """
-        logging.warning(failure.value)
-
-
-# ===============================================
-# Reference: https://stackoverflow.com/a/43661172/15503925
-# ===============================================
 def runSpider():
     # Load passing data from post
     postData = json.loads(request.body.read())
@@ -49,14 +15,52 @@ def runSpider():
     if result['status'] is True:
         keyword = postData['keyword']
 
-        process = Process(target=getUserTweets, args=[keyword])
-        process.start()
+        params = {
+            'by': 'relevancy',
+            'keyword': keyword,
+            'limit': '100',
+            'newest': '0',
+            'official_mall': '1',
+            'order': 'desc',
+            'page_type': 'search',
+            'scenario': 'PAGE_MALL_SEARCH',
+            'version': '2',
+        }
+        url = 'https://shopee.co.id/api/v4/search/search_items?'
 
-        # ===============================================
-        # Remove comment code bellow, response will
-        # wait until the spider finish
-        # ===============================================
-        process.join()
+        response = requests.get(url, params=params)
+        json_obj = json.loads(response.text)
+
+        products = []
+
+        for i in json_obj['items']:
+            product = {}
+            
+            item_id = i['item_basic']['itemid']
+            item_name = i['item_basic']['name']
+            shop_id = i['item_basic']['shopid']
+            item_brand = i['item_basic']['brand']
+            detail_url = 'https://shopee.co.id/{}-i.{}.{}'.format(item_name.replace(' ','-'), shop_id, item_id)
+
+            if 'None' == item_brand:
+                item_brand = None
+
+            product['item_id'] = item_id
+            product['item_name'] = item_name
+            product['item_detail_url'] = detail_url
+            product['item_photo_url'] = 'https://cf.shopee.co.id/file/{}'.format(i['item_basic']['image'])
+            product['item_stock'] = i['item_basic']['stock']
+            product['item_brand'] = item_brand
+            product['item_price'] = i['item_basic']['price']
+            product['item_rating_star'] = i['item_basic']['item_rating']['rating_star']
+            product['is_official_shop'] = i['item_basic']['is_official_shop']
+            product['shop_location'] = i['item_basic']['shop_location']
+            product['shop_id'] = shop_id
+            product['total_items_sold'] = i['item_basic']['historical_sold']
+            
+            products.append(product)
+        
+        result['data'] = products
 
     return result
 
@@ -81,6 +85,7 @@ if __name__ == '__main__':
 
     @app.post('/runspider')
     def run_spider():
+        response.content_type = 'application/json'
         return runSpider()
 
     app.run(host=API_HOST, port=API_PORT, debug=API_DEBUG)
